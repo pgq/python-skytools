7798f6e5c9a8b022160b4dc70ec394eb1bbc4149
Marko Kreen <markokr@gmail.com>
2016-06-07 23:57:16 +0300

    setup: use setuptools, gets wheel support

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
 import sys
 
@@ -22,10 +22,24 @@ setup(
     name = "skytools",
     license = "ISC",
     version = '3.3',
+    url = "https://github.com/pgq/python-skytools",
     maintainer = "Marko Kreen",
     maintainer_email = "markokr@gmail.com",
     description = "SkyTools - tools for PostgreSQL",
-    platforms = "POSIX, MacOS, Windows",
     packages = ['skytools'],
     ext_modules = c_modules,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: ISC License (ISCL)",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Topic :: Database",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ],
 )
