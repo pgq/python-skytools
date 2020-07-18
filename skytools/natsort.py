4dcbb40911fc603bdb8cc8c48dac35eb3bfe1047
Marko Kreen <markokr@gmail.com>
2020-07-18 14:39:41 +0300

    Fix natsort on py3, improve rules

--- skytools/natsort.py
+++ skytools/natsort.py
@@ -1,15 +1,24 @@
 """Natural sort.
 
 Compares numeric parts numerically.
+
+Rules:
+
+* String consists of numeric and non-numeric parts.
+* Parts are compared pairwise, if both are numeric then numerically,
+  otherwise textually.  (Only first fragment can have different type)
+
+Extra rules for version numbers:
+
+* In textual comparision, '~' is less than anything else, including end-of-string.
+* In numeric comparision, numbers that start with '0' are compared as decimal
+  fractions, thus number that starts with more zeroes is smaller.
+
 """
 
 import re as _re
 
-# Based on idea at http://code.activestate.com/recipes/285264/
-# Works with both Python 2.x and 3.x
-# Ignores leading zeroes: 001 and 01 are considered equal
-
-_rc = _re.compile(r'\d+|\D+')
+_rc = _re.compile(r'\d+|\D+', _re.A)
 
 __all__ = (
     'natsort_key', 'natsort', 'natsorted',
@@ -18,8 +27,24 @@ __all__ = (
 
 
 def natsort_key(s):
-    """Split string to numeric and non-numeric fragments."""
-    return [not f[0].isdigit() and f or int(f, 10) for f in _rc.findall(s)]
+    """Returns tuple that sorts according to natsort rules.
+    """
+    # key consists of triplets (type:int, magnitude:int, value:str)
+    key = []
+    if '~' in s:
+        s = s.replace('~', '\0')
+    for frag in _rc.findall(s):
+        if frag < '0':
+            key.extend((1, 0, frag + '\1'))
+        elif frag < '1':
+            key.extend((2, len(frag.lstrip('0')) - len(frag), frag))
+        elif frag < ':':
+            key.extend((2, len(frag), frag))
+        else:
+            key.extend((3, 0, frag + '\1'))
+    if not key or key[-3] == 2:
+        key.extend((1, 0, '\1'))
+    return tuple(key)
 
 
 def natsort(lst):
@@ -30,9 +55,7 @@ def natsort(lst):
 def natsorted(lst):
     """Return copy of list, sorted in natural order, case-sensitive.
     """
-    lst = lst[:]
-    natsort(lst)
-    return lst
+    return sorted(lst, key=natsort_key)
 
 
 # case-insensitive api
@@ -50,7 +73,5 @@ def natsort_icase(lst):
 def natsorted_icase(lst):
     """Return copy of list, sorted in natural order, case-sensitive.
     """
-    lst = lst[:]
-    natsort_icase(lst)
-    return lst
+    return sorted(lst, key=natsort_key_icase)
 
