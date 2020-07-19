bda39cca3595250cad552d36f6a2d68f247e1aaf
Marko Kreen <markokr@gmail.com>
2020-07-19 12:28:10 +0300

    setup: load description from readme

--- setup.py
+++ setup.py
@@ -12,29 +12,35 @@ with open("skytools/installer_config.py") as f:
             _version = ln.split()[2].strip("\"'")
 len(_version)
 
-# don't build C module on win32 as it's unlikely to have dev env
+# load info
+with open("README.rst") as f:
+    ldesc = f.read().strip()
+    sdesc = ldesc.split("\n")[0].split("-", 1)[-1].strip()
+
+# don"t build C module on win32 as it"s unlikely to have dev env
 BUILD_C_MOD = 1
-if sys.platform == 'win32':
+if sys.platform == "win32":
     BUILD_C_MOD = 1
 
 # check if building C is allowed
 c_modules = []
 if BUILD_C_MOD:
     c_modules = [
-        Extension("skytools._cquoting", ['modules/cquoting.c']),
-        Extension("skytools._chashtext", ['modules/hashtext.c']),
+        Extension("skytools._cquoting", ["modules/cquoting.c"]),
+        Extension("skytools._chashtext", ["modules/hashtext.c"]),
     ]
 
 # run actual setup
 setup(
     name="skytools",
-    license="ISC",
+    description="Utilities for database scripts",
+    long_description=ldesc,
     version=_version,
+    license="ISC",
     url="https://github.com/pgq/python-skytools",
     maintainer="Marko Kreen",
     maintainer_email="markokr@gmail.com",
-    description="Utilities for database scripts",
-    packages=['skytools'],
+    packages=["skytools"],
     ext_modules=c_modules,
     zip_safe=False,
     classifiers=[
@@ -45,7 +51,6 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: Microsoft :: Windows",
         "Operating System :: POSIX",
-        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
         "Topic :: Database",
         "Topic :: Software Development :: Libraries :: Python Modules",
