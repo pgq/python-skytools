85bc85d93b50246fa0d41506cf344dbeef67c560
Marko Kreen <markokr@gmail.com>
2020-08-02 09:36:00 +0300

    parsing: add tests

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,9 +1,10 @@
 
 import pytest
+
 from skytools.parsing import (
     dedent, hsize_to_bytes, merge_connect_string, parse_acl,
     parse_connect_string, parse_logtriga_sql, parse_pgarray,
-    parse_statements, parse_tabbed_table, sql_tokenizer,
+    parse_sqltriga_sql, parse_statements, parse_tabbed_table, sql_tokenizer,
 )
 
 
@@ -20,11 +21,21 @@ def test_parse_pgarray():
         parse_pgarray('[1]=}')
     with pytest.raises(ValueError):
         parse_pgarray('{"..." , }')
+    with pytest.raises(ValueError):
+        parse_pgarray('{"..." ; }')
+    with pytest.raises(ValueError):
+        parse_pgarray('{"}')
+    with pytest.raises(ValueError):
+        parse_pgarray('{"..."}zzz')
+    with pytest.raises(ValueError):
+        parse_pgarray('{"..."}z')
 
 
 def test_parse_sqltriga_sql():
     # Insert event
     row = parse_logtriga_sql('I', '(id, data) values (1, null)')
+    assert row == {'data': None, 'id': '1'}
+    row = parse_sqltriga_sql('I', '(id, data) values (1, null)', pklist=["id"])
     assert row == {'data': None, 'id': '1'}
 
     # Update event
@@ -40,18 +51,69 @@ def test_parse_sqltriga_sql():
     assert keys == {}
     assert row == {'data': None, 'id': '1'}
 
+    keys, row = parse_logtriga_sql('I', '(id, data) values (1, null)', splitkeys=True)
+    assert keys == {}
+    assert row == {'data': None, 'id': '1'}
+
     # Update event, splitkeys
     keys, row = parse_logtriga_sql('U', "data='foo' where id = 1", splitkeys=True)
     assert keys == {'id': '1'}
     assert row == {'data': 'foo'}
+    keys, row = parse_logtriga_sql('U', "data='foo',type=3 where id = 1", splitkeys=True)
+    assert keys == {'id': '1'}
+    assert row == {'data': 'foo', 'type': '3'}
 
     # Delete event, splitkeys
     keys, row = parse_logtriga_sql('D', "id = 1 and id2 = 'str''val'", splitkeys=True)
     assert keys == {'id': '1', 'id2': "str'val"}
 
+    # generic
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('J', "(id, data) values (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id) values (1, null)")
+
+    # insert errors
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "insert (id, data) values (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id; data) values (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id, data) select (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id, data) values of (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id, data) values (1; null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('I', "(id, data) values (1, null) ;")
+    with pytest.raises(ValueError, match="EOF"):
+        parse_logtriga_sql('I', "(id, data) values (1, null) , insert")
+
+    # update errors
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('U', "(id,data) values (1, null)")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('U', "id,data")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('U', "data='foo';type=3 where id = 1")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('U', "data='foo' where id>1")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('U', "data='foo' where id=1 or true")
+
+    # delete errors
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('D', "foo,1")
+    with pytest.raises(ValueError):
+        parse_logtriga_sql('D', "foo = 1 ,")
+
 
 def test_parse_tabbed_table():
     assert parse_tabbed_table('col1\tcol2\nval1\tval2\n') == [
+        {'col1': 'val1', 'col2': 'val2'}
+    ]
+    # skip rows with different size
+    assert parse_tabbed_table('col1\tcol2\nval1\tval2\ntmp\n') == [
         {'col1': 'val1', 'col2': 'val2'}
     ]
 
@@ -122,6 +184,8 @@ def test_dedent():
 def test_hsize_to_bytes():
     assert hsize_to_bytes('10G') == 10737418240
     assert hsize_to_bytes('12k') == 12288
+    with pytest.raises(ValueError):
+        hsize_to_bytes("x")
 
 
 def test_parse_connect_string():
