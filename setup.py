cb22a18cc4f807f9b83f93d8459f20e58394e272
Marko Kreen <markokr@gmail.com>
2023-08-25 21:47:51 +0300

    setup: build abi3 wheel

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 """Setup for skytools module.
 """
 
+from typing import Tuple
 from setuptools import Extension, setup
 
-# use only stable abi
-define_macros = [('Py_LIMITED_API', '0x03070000')]
-py_limited_api = True
+try:
+    from wheel.bdist_wheel import bdist_wheel
+    class bdist_wheel_abi3(bdist_wheel):
+        def get_tag(self) -> Tuple[str, str, str]:
+            python, abi, plat = super().get_tag()
+            if python.startswith("cp"):
+                return CP_VER, "abi3", plat
+            return python, abi, plat
+    cmdclass = {"bdist_wheel": bdist_wheel_abi3}
+except ImportError:
+    cmdclass = {}
 
-# run actual setup
+CP_VER = "cp37"
+API_VER = ('Py_LIMITED_API', '0x03070000')
+
 setup(
+    cmdclass = cmdclass,
     ext_modules = [
         Extension("skytools._cquoting", ["modules/cquoting.c"],
-                  define_macros=define_macros, py_limited_api=py_limited_api),
+                  define_macros=[API_VER], py_limited_api=True),
         Extension("skytools._chashtext", ["modules/hashtext.c"],
-                  define_macros=define_macros, py_limited_api=py_limited_api),
+                  define_macros=[API_VER], py_limited_api=True),
     ]
 )
 
